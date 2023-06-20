a1b330a9449413f6418aeca49831be20371e4e19
Housam Ramadan <Housamsoma99@gmail.com>
2023-06-20 16:51:19 +0300

    tbc

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ dotenv.config();
 import { client } from './utils/client.js';
 import fs from 'fs';
 import path from 'path';
-
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const eventsPath = path.join(__dirname.replace(/^\/([a-z]):\//i, '$1:/'), 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-  const event = await import(`./events/${file}`);
-  client.on(event.default.name, event.default.run.bind(null, client));
+for (const file of eventFiles) {;
+	const filePath = path.join(eventsPath, file);
+  await import(`file://${filePath}`);
 }
-
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN)
